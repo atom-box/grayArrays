135ad71e45f9d9545a7dade6b2429445a6aef5c3
Evan Genest <genest.1@osu.edu>
2018-11-28 07:14:39 -0600

    Wrap logic in an IIFE..

--- sleepIn.js
+++ sleepIn.js
@@ -1,10 +1,9 @@
 
-
-
+(function(weekday, vacation){
 if(!weekday || vacation){return true } else if (weekday && !vacation ){return false } else { return "Uncaught error."}
+} )();
 
-
-/*
+/*  Wrap function in an IIFE.
 Nov 28, 2018  Evan Genest
 from https://codingbat.com/prob/p187868
 The parameter weekday is true if it is a weekday, and the parameter vacation is true if we are on vacation. We sleep in if it is not a weekday or we're on vacation. Return true if we sleep in.
