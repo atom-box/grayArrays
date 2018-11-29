45f7201e0af9753d68b26e4c637955f17dea985e
Evan Genest <genest.1@osu.edu>
2018-11-29 07:46:49 -0600

    Add slice function.

---
+++ meritRegex.js
@@ -0,0 +1,178 @@
+
+
+/* Awesome backtick syntax: allows multiline string! And escaped some quote marks!
+Won't hoist IF CONST 
+If VAR sort of hoists.  Just mumbles 'undefined', as opposed to full on crash of CONST*/
+
+var mb = `Citizenship
+   in the nation
+       BOY SCOUTS OF AMERICA
+          MERIT BADGE SERIES
+
+Citizenship in
+ the Nation
+
+“Enhancing our youths’ competitive edge through merit badges”
+                Requirements
+                     1. Explain what citizenship in the nation means and what
+                        it takes to be a good citizen of this country. Discuss the
+                        rights, duties, and obligations of a responsible and active
+                        American citizen.
+
+                     2. Do TWO of the following:
+                        a.  Visit a place that is listed as a National Historic
+                            Landmark or that is on the National Register of Historic
+                            Places. Tell your counselor what you learned about
+                            the landmark or site and what you found interesting
+                            about it.
+                        b.  Tour your state capitol building or the U.S. Capitol.
+                            Tell your counselor what you learned about the capitol,
+                            its function, and its history.
+                        c.  Tour a federal facility. Explain to your counselor what
+                            you saw there and what you learned about its function
+                            in the local community and how it serves this nation.
+                        d.  Choose a national monument that interests you. Using
+                            books, brochures, the Internet (with your parent’s
+                            permission), and other resources, find out more about
+                            the monument. Tell your counselor what you learned,
+                            and explain why the monument is important to this
+                            country’s citizens.
+
+                     3. Watch the national evening news five days in a row OR
+                        read the front page of a major daily newspaper five days
+                        in a row. Discuss the national issues you learned about
+                        with your counselor. Choose one of the issues and explain
+                        how it affects you and your family.
+
+35871
+ISBN 978-0-8395-3248-4
+©2014 Boy Scouts of America
+2014 Printing
+4. Discuss each of the following documents with your
+   counselor. Tell how you feel life in the United States might
+   be different without each one. Then choose one document
+   and explain how it impacts you and your family.
+   a. D eclaration of Independence
+   b. P reamble to the Constitution
+   c. T he Constitution
+   d. B ill of Rights
+   e. Amendments to the Constitution
+
+5. List the six functions of government as noted in the
+   preamble to the Constitution. Discuss with your
+   counselor how these functions affect your family and
+   local community.
+
+6. With your counselor’s approval, choose a speech of
+   national historical importance. Find out about the author,
+   and tell your counselor about the person who gave the
+   speech. Explain the importance of the speech at the time
+   it was given, and tell how it applies to American citizens
+   today. Choose a sentence or two from the speech that has
+   significant meaning to you, and tell your counselor why.
+
+7. Name the three branches of our federal government and
+   explain to your counselor their functions. Explain how
+   citizens can be involved in each branch. Explain the impor-
+   tance of our checks and balances system. Describe how
+   each branch “checks” and “balances” the others.
+
+8. Name your two senators and a member of
+   Congress from your congressional district. Write a
+   letter about a national issue and send it to one of these
+   elected officials, sharing your view with him or her.
+   Show your letter to your counselor, along with any
+   response you might receive.
+
+                                                         Citizenship in The Nation    3
+Resources.
+
+Resources
+Scouting Literature                         Krull, Kathleen. A Kid’s Guide to
+The Constitution of the United States;         America’s Bill of Rights: Curfews,
+American Business, American Cultures,          Censorship, and the 100-Pound
+American Heritage, Citizenship in the          Giant. William Morrow & Co., 1999.
+Community, Citizenship in the World,        Mackintosh, Barry. The National
+and Law merit badge pamphlets                  Park Service. Chelsea House
+                                               Publishers, 1988.
+ For more information about
+ Scouting-related resources, visit          Maestro, Betty. A More Perfect Union:
+ the BSA’s online retail catalog               The Story of Our Constitution.HarperCollins, 2008.
+ (with your parent’s permission)
+ at http://www.scoutstuff.org.              McHugh, Erin. National Parks: A
+                                               Kid’s Guide to America’s Parks,
+                                               Monuments and Landmarks. Black
+Books                                          Dog & Leventhal, 2012.
+Feinberg, Barbara Silberdick. The           McIntire, Suzanne, ed. The American
+  Articles of Confederation: The First         Heritage Book of Great American
+  Constitution of the United States.           Speeches for Young People. Jossey-
+  Twenty-First Century Books, 2002.            Bass, 2001.
+Freedman, Russell. Give Me Liberty!         Panchyk, Richard. Keys to American
+  The Story of the Declaration of              History: Understanding Our Most
+  Independence. Holiday House, 2000.           Important Historic Documents.
+Jaffe, Steven H. Who Were the Founding         Chicago Review Press, 2009.
+  Fathers? Two Hundred Years of             Selzer, Adam. The Smart Aleck’s Guide
+  Reinventing American History.                to American History. Delacorte
+  Henry Holt and Co., 1996.                    Books for Young Readers, 2009.
+Kassinger, Ruth. U.S. Census: A Mirror      Zeinert, Karen. Free Speech: From
+  of America. Raintree Steck-Vaughn            Newspapers to Music Lyrics.
+  Publishers, 2000.                            Enslow Publishers, 1995.
+
+46    Citizenship in The Nation
+                                                                     .Resources
+
+Organizations and Websites               U.S. House of Representatives
+American Civil Liberties Union           Telephone: 202-224-3121
+125 Broad St., 18th Floor                Website: http://www.house.gov
+New York, NY 10004                       U.S. Government Printing Office
+Telephone: 212-549-2500                  Telephone: 202-512-0018
+Website: http://www.aclu.org             Website: http://www.gpoaccess.gov
+Congress.gov                             U.S. Senate
+Website: http://www.congress.gov         Telephone: 202-224-3121
+Democratic National Committee            Website: http://www.senate.gov
+Telephone: 202-863-8000                  U.S. Supreme Court
+Website: http://www.democrats.org        Telephone: 202-479-3000
+GovSpot                                  Website: http://www.supremecourt.gov
+Website: http://www.govspot.com          USA.gov
+League of Women Voters                   Website: http://www.usa.gov
+Telephone: 202-429-1965                  The White House
+Website: http://www.lwv.org              1600 Pennsylvania Ave. NW
+National Constitution Center             Washington, DC 20500
+525 Arch St.                             Telephone: 202-456-1414
+Independence Mall                        Website: http://www.whitehouse.gov
+Philadelphia, PA 19106
+Telephone: 215-409-6600                 Acknowledgments
+Website:                                 For this new edition of the Citizenship
+http://www.constitutioncenter.org        in the Nation merit badge pamphlet,
+National Park Service                    the Boy Scouts of America thanks Harry
+1849 C St. NW                            C. Boyte, Center for Democracy and
+Washington, DC 20240                     Citizenship, Humphrey Institute, for
+Telephone: 202-208-6843                  sharing his time, expertise, and assistance.
+Website: http://www.nps.gov                  The Boy Scouts of America is grate-
+Republican National Committee            ful to the men and women serving on
+Telephone: 202-863-8500                  the Merit Badge Maintenance Task Force
+Website: http://www.rnc.org              for the improvements made in updating
+                                         this pamphlet.
+U.S. Census Bureau
+4600 Silver Hill Road
+Washington, DC 20233-4600
+Telephone: 301-763-4636
+Website: http://www.census.gov
+The U.S. Constitution Online
+Website: http://www.usconstitution.net`
+/*-------------------------------------*/
+console.log("Find ");
+let x = mb.slice(13,23);
+console.log(x);
+console.log("Find ");
+console.log("Find ");
+console.log("Find ");
+console.log("Find ");
+
+
+
+/*-------------------------------------*/
+/*   
+Nov 29, 2018  Evan Genest
+from https://filestore.scouting.org/filestore/Merit_Badge_ReqandRes/Citizenship_in_the_Nation.pdf
+*/
