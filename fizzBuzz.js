2c65d48e36d331eaf431d06ac62575ec918ffa91
Evan Genest <genest.1@osu.edu>
2018-12-01 10:04:05 -0600

    1st.  Just header there.

---
+++ fizzBuzz.js
@@ -0,0 +1,12 @@
+console.log("divisible";
+
+
+
+
+/*-------------------------------------*/
+/*   
+Dec 1, 2018  Evan Genest
+Fizz Buzz
+Print out all the numbers from 1 to 100. But for every number divisible by 3 print replace it with the word “Fizz,” for any number divisible by 5 replace it with the word “Buzz” and for a number divisible by both 3 and 5 replace it with the word “FizzBuzz.”
+
+ */
