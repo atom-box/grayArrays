4eed422a77a13d0fd1ae6299e48c6481f9be54c5
Evan Genest <genest.1@osu.edu>
2018-12-01 13:18:43 -0600

    Make type checkers work.  Make multi-d array.

---
+++ sumArrays.js
@@ -0,0 +1,20 @@
+const total = 0;
+const w = [2,[77, 200,[100, 35],[230, 210, 185, 172], 29, 45], 3.14, 2.7, [1965, 1960, 1956], null, undefined];
+const x = [33, 45, 78];
+
+console.log("Is w an Array?");
+console.log(Array.isArray(w));
+
+console.log("Is w Evan a String?");
+console.log(typeof "Evan");
+
+console.log("Is null null?");
+console.log(null ===  null);
+
+console.log("Done.");
+
+/*   
+Dec 1, 2018  Evan Genest
+This problem asks you to sum up all of the numbers within an array, but the array may also contains other arrays with numbers. This is what we call a nested array. For example:
+[1, 1, 1, [3, 4, [8]], [5]]
+ */
