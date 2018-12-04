e974b2a7b56e0c415a1ce92e1d4e5cbe6cac057a
Evan Genest <genest.1@osu.edu>
2018-12-03 20:37:56 -0700

    Resorted to  answer from book.

--- sumArrays.js
+++ sumArrays.js
@@ -4,35 +4,20 @@ const v = [2,[77, 200,[100, 35],[230, 210, 185, 172], 29, 45]];
 const w = [2,[77, 200,[100, 35],[230, 210, 185, 172], 29, 45], 3.14, 2.7, [1965, 1960, 1956], null, undefined];
 const x = [33, 45, 78];
 
-console.log("Is w an Array?");
-console.log(Array.isArray(w));
 
-let total = 0;
+function sumNested(arr) { let result = 0;
+// sum up all the numbers in array
+for (let i = 0; i < arr.length; i++) {
+// if element is a nested array, sum all of its elements
+if (typeof arr[i] !== 'number') { result += sumNested(arr[i]);
+} else {
+result += arr[i];
+} }
+return result; }
 
-function tally (tot_1, arr1 ){
-	if (tot_1===NaN || !Array.isArray(arr1)){
-		return 0;
-		//	non-array was passed in
-	}
-	let stop = arr1.length;
-	let i = 0;
-	for(; i < stop; i++){
-		if (Array.isArray(arr1[i])){
-			tot_1 += tally(arr1[i]);
-
-		} else {
-			tot_1 += arr1[i];
-			console.log("added " + arr1[i] + " in the ELSE");
-		}
-		console.log("after func added");
-	} 
-	return tot_1;
-}
-
-console.log("Simple array totalled: " + tally(total, x));
-console.log("Multi-array U totalled: " + tally(total, u));
-console.log("Multi-array totalled: " + tally(total, w));
-console.log("Done.");
+console.log("---Simple array totalled: " + sumNested(x));
+console.log("---Multi-array U totalled: " + sumNested(u));
+console.log("---Done.");
 
 /*   
 Dec 1, 2018  Evan Genest
