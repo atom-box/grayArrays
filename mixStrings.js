2cbc57639accf98cbc278928fd1c72a286a8618c
Evan Genest <genest.1@osu.edu>
2018-12-14 07:02:39 -0600

    Add control statements
    Structure of control is
    WHILE ( a || b ) , not AND
    IF ()
    concat
    IF ()
    concat

--- mixStrings.js
+++ mixStrings.js
@@ -1,9 +1,22 @@
 const chopify =(str1, str2)=>{
-	let str1and2 = str1 + str2;
+	let chars1 = str1.split('');
+	let chars2 = str2.split('');
+	let str1and2 = '';
+	while (chars1.length || chars2.length){
+		if (chars1.length){
+			str1and2 += chars1.shift(); 
+		}
+		if (chars2.length){
+			str1and2 += chars2.shift(); 
+		}
+	}
 	return str1and2; 
 };
 
 console.log(chopify("Pig", "Pen"));
+console.log(chopify("Laertes", "Saw the best Hamilton production"));
+
+
 
 /**
 codingbat.com says:
@@ -17,4 +30,5 @@ mixString("Hi", "There") → "HTihere"
 mixString("xxxx", "There") → "xTxhxexre"
 December 14, 2018
 @mistergenest
+Dev Diary:  This took 30 minutes: had 3 things to fix that I fixed immediately.   Reasonable for now.
 */
