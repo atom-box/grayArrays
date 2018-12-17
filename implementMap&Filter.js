ad24aacca5665915ab9c3eeb70599a7eb48f51b9
Evan Genest <genest.1@osu.edu>
2018-12-17 07:36:20 -0600

    Add assignment instructions.

---
+++ implementMap&Filter.js
@@ -0,0 +1,12 @@
+
+
+/**
+Coding Interview byte-ebook says:
+IMPLEMENT MAP  &  FILTER
+They are both functions that take in a list, perform some operation on that list without changing the original list, and then return a new lists. The functions do not change any other variables and do not touch anything else except those lists they were given.
+Map: applY the function to each element in the list   ARGS: list and function
+Filter returns a new list where every element in the original list passes the conditional (returns true).  ARGS: list and conditional
+December 17, 2018
+@mistergenest
+Dev Diary:  7:28am
+*/
