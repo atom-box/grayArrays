5c3938b7aa06a76b7a6ce6c0f44d897624c6c929
Evan Genest <genest.1@osu.edu>
2019-01-04 08:02:46 -0600

    Restore point, insufficientFunds feature incomplete.

--- makeBricks.js
+++ makeBricks.js
@@ -9,7 +9,7 @@ Then follow the inline todos.
 
 
 /* 
-brix  An object with # of bricks and boolean function for wall length.
+brix  Object.  Contains property # of stockpiled , contains a setter method, contains a buildthewall-type method.
 _stockpile Object.  A private property.  Each category of brick is named by inches a digitstring
 */ 
 let brix = {};
@@ -20,7 +20,8 @@ brix = {
 	}
 };
 
-// g & s Integers.  New total stock of bricks.
+// g & s Integers.  
+// Sets the stockpile of Grand & Small bricks
 brix.set =(g, s)=>{
 	if (!Number.isInteger(g) || !Number.isInteger(s)){return}
 	if (g > 0){
@@ -37,13 +38,12 @@ brix.set =(g, s)=>{
  manualLarge Integer. Optional parameter: if passed in, resets the inventory of large bricks.
  manualSmall Integer.  Optional parameter: if passed in, resets the inventory of small bricks.
 */
+
 brix.postBuildGap =(currentGap, whichBrick)=> {
 	console.log(`If your opening is ${currentGap} and your brick size is ${whichBrick}...`);
 
-	// to do: Input should be vetted.
-	// to do: Brick stockpiles should be confirmed.
 	// a super-function will call the minifunctions
-	// then STOP!   This was 20 min codingbat online...DevDiary: adding just a few features was 80% of the time.  The core was nothing.  Like a real project.
+	// then STOP!   
 
 	// Is gap already solved? Report as zero.
 	if (currentGap === 0) return currentGap;
@@ -57,11 +57,10 @@ brix.postBuildGap =(currentGap, whichBrick)=> {
 // Required before passing to array dereference.
 let whichBrickStr = String(whichBrick);
 
-// To do!!!: function here to compare canLay to brix._stockpile[whichBrickStr] and output a new int called actualLay !!!
+// To do!!!: function here to compare canLay to brix._stockpile[whichBrickStr] and output a new int called actualLay !!!  This passes fullreq, reduced, or zero to the next function, and maybe a console.log for each now
 
 
-
-	// to do: Confirm bricks exist in stock!  Strategy: if insufficient funds, lay nought, return unchanged gap.  If sufficient funds, subtract bricks from stockpile.
+// not neccessary, redundant to the previous
 	if (false){
 		brix._stockpile[whichBrickStr] -= canLay;
 	console.log(`Laid ${canLay} (size ${whichBrick}) bricks.  And left a space of ${leftOverSpace}.`);
@@ -95,4 +94,5 @@ makeBricks(3, 1, 9) → false
 makeBricks(3, 2, 10) → true
 
 DevDiary: I learned that arrow functions are different than functions.  They are strictish, they lack the arguments list, so you cannot check their length.
+DevDiary: This was supposedly a 20 min codingbat online...but I couldn't resist making it 'real' with input checking, an inventory to manipulate adding just a few features was 80% of the time.  The original core was nothing -- feature-creep occurred like crazy, like in a real project.
 */
