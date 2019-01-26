73e36f5d1d0aed4ecbfbd4be13e1a6530c279326
Evan Genest <genest.1@osu.edu>
2019-01-26 05:39:24 -0600

    Add instructions.

---
+++ arrayInArray.js
@@ -0,0 +1,6 @@
+/*   
+Jan 26, 2019  Evan Genest
+Same problem from a month ago; see if you can remember the recursive base case.
+This problem asks you to sum up all of the numbers within an array, but the array may also contains other arrays with numbers. This is what we call a nested array. For example:
+[1, 1, 1, [3, 4, [8]], [5]]
+ */
