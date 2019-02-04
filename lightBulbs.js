cf557b43c5e04441a24d81f1aed1ab764191cd2c
atom-box <atom_box@hotmail.com>
2019-02-04 09:42:37 -0600

    Make array-former function.

--- lightBulbs.js
+++ lightBulbs.js
@@ -1,3 +1,26 @@
+let bulbs = [];
+
+let makeClones =(size, type)=>{
+	let things = [];
+	for( ; size > 0; size--){
+		// offset b/c zero indexed
+		things[size -1] = type;
+	}
+	return things;
+}
+
+
+
+let foo = makeClones(100, false);
+console.log(`Array is ${foo.length} long and looks like this:`)
+console.log(foo);
+
+
+
+
+
+
+
 /**
 SWITCHING LIGHTBULBS PROBLEM
 There are 100 light bulbs, labeled from 1 to 100, lined up all set to off initially. There are also 100 people each numbered 1 to 100 as well. Person 1 will go through all the light bulbs and flip the switch turning all of them on. Then person number 2 will go through all the light bulbs and flip the switch on each 2nd element turning them off, namely: light bulbs #2, #4, #6, #8, etc. Then person 3 will go and do the same for the 3rd ligh bulb, 6th, 9th, etc. Then questions are usually asked about the light bulbs, for example:
