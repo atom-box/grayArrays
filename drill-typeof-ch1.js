d6b68d92830173050795130c3f5e05917fcb2751
Evan Genest <genest.1@osu.edu>
2019-02-13 08:09:42 -0600

    Add a definition

--- drill-typeof-ch1.js
+++ drill-typeof-ch1.js
@@ -12,7 +12,24 @@ console.log(reflect instanceof Function);
 console.log(reflect instanceof Object);
 
 
+var x = new String("foo");
+var y = "bar";
+
+/**Much love to the No Starch press and Nicholas Zakas.
+This is the most vanilla of vanilla JS guides.
+Real fundamentals.*/
+
+
 /**
 From Nicolas Zakas, Ch 1
 @mistergenest February 13, 2018
+*/
+
+/**
+You can create new objects by using
+the new operator
+or
+the REFERENCE LITERAL
+var x = new String("foo");
+var y = "bar";
 */
