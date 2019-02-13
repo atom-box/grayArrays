76f2610b487ee516ab173dfd00688011157db035
Evan Genest <genest.1@osu.edu>
2019-02-13 07:43:44 -0600

    Add Examples from 'Object Oriented Javascript'.

---
+++ drill-typeof-ch1.js
@@ -0,0 +1,18 @@
+let nums = [];
+let struct = {};
+let reflect = x =>{
+	return x;
+}
+
+console.log(nums instanceof Array);
+console.log(nums instanceof Object);
+console.log(struct instanceof Object);
+console.log(struct instanceof Array);
+console.log(reflect instanceof Function);
+console.log(reflect instanceof Object);
+
+
+/**
+From Nicolas Zakas, Ch 1
+@mistergenest February 13, 2018
+*/
