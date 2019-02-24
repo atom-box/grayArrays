af4afe781d708b22693dc4ec651e2c95a023b6f7
Evan Genest <genest.1@osu.edu>
2019-02-24 05:56:01 -0600

    Add tigers data arrays: 3B & G.

---
+++ modeTigers.js
@@ -0,0 +1,27 @@
+
+
+
+
+
+/**
+Return the mean, median, and mode of an array.
+*/
+
+/**
+February 24, 2019
+Evan Genest
+*/
+
+
+
+
+/* Detroit Tigers stats from 2016*/
+// watch these hoist or not
+let tigers = {games: games,
+							triples: triples
+							}
+
+let games = [105, 158, 153, 137, 110, 153, 94, 120, 154, 92, 109, 68, 56, 30, 31, 30, 29, 13, 5, 8, 3, 1, 35, 34, 19, 14, 26, 50, 6, 14, 37, 61, 56, 5, 20, 19, 62, 4, 21, 3, 6, 66, 24, 54];
+const triples = [1, 1, 4, 0, 4, 2, 5, 2, 0, 1, 2, 1, 3, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+
