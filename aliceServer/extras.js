ffe77e97d91f6847ef04e06b943f19a2623df29b
Evan Genest <genest.1@osu.edu>
2019-02-25 13:02:53 -0600

    Restore point, pseudocode half implemented in mode function.

--- aliceServer/extras.js
+++ aliceServer/extras.js
@@ -1,17 +1,31 @@
 const mode = (nums)=>{
+	let champIndex = 0,
+	occurances = 0,
+	maxCount = 0;
+
+	nums.sort();
+
+	for (let n of nums){
+
+	}
 	/**
 	STRATEGY
 	sort the array!!
+
 	set index of champ, index of now,  flood, occurances.
 	whileloop increments occurances until hetero
 	if occurances > champ set new index of champ as that.
 	Return nums[champIndex]
 
+................LEFT OFF HERE
+
+	store prev
+	if same as prev
+	increment OCCURANCES
+
  ALTERNATIVE STRATEGY write pairs to [a,0],[b,0],[c,0].  
  This avoids sorting.  Would it be better at scale?  Prolly not.
 	*/
-	let champ = null;
-	let occurances = 0;
 };
 
 const dummy =()=>{
