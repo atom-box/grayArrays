857fb4526856362d86242907d3fa056a35807083
Evan Genest <genest.1@osu.edu>
2019-02-25 12:40:30 -0600

    Move MODE function to a module.

---
+++ aliceServer/extras.js
@@ -0,0 +1,19 @@
+const mode = (nums)=>{
+	/**
+	STRATEGY
+	sort the array!!
+	set index of champ, index of now,  flood, occurances.
+	whileloop increments occurances until hetero
+	if occurances > champ set new index of champ as that.
+	Return nums[champIndex]
+
+ ALTERNATIVE STRATEGY write pairs to [a,0],[b,0],[c,0].  
+ This avoids sorting.  Would it be better at scale?  Prolly not.
+	*/
+	let champ = null;
+	let occurances = 0;
+};
+
+
+
+export
