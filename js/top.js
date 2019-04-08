b6775a7396a6714d0459069d2e4161f2fe84e710
Evan Genest <genest.1@osu.edu>
2019-04-08 07:11:36 -0500

    Add the JOIN example div.

--- js/top.js
+++ js/top.js
@@ -1,14 +1,44 @@
-import {dummy, jr} from './bottom.js';
+import {dummy, jr, rotators} from './bottom.js';
 // to avoid CORS error, get path right
 // THE file that is the head can be anywhere, but the next called path must have same insert point on the tree.
 // See also SWISSREGEX project
 
 
+let cliffWords = ["Lear", "addresses", "these", "words", "to", "the", "body", "of", "Cordelia", "in", "the", "play’s", "final", "scene"];
+let bardWords = ["O", "thou'lt", "come", "no", "more", "never", "never", "never", "never", "never"];
+
+  //------------------------------------------------
+let ele = document.createElement("P");         // Create a <p> element
+let cont = `${cliffWords} -AND- ${bardWords}`;
+ele.innerHTML = cont;            // Insert text
+document.getElementById("demo1__before").appendChild(ele); 
+ele = document.createElement("P");         // Create a <p> element
+cont = cliffWords.concat(bardWords);
+ele.innerHTML = cont;           // Insert text
+document.getElementById("demo1__after").appendChild(ele); 
+  //------------------------------------------------
+ele = document.createElement("P");         // Create a <p> element
+cont = `${bardWords}`
+ele.innerHTML = cont;            // Insert text
+document.getElementById("demo2__before").appendChild(ele); 
+ele = document.createElement("P");         // Create a <p> element
+cont = bardWords.slice(5, 9);
+ele.innerHTML = cont;           // Insert text
+document.getElementById("demo2__after").appendChild(ele); 
+  //------------------------------------------------
+ele = document.createElement("P");         // Create a <p> element
+let lear_tears = bardWords.slice(5, 9);
+cont = `${lear_tears}`
+ele.innerHTML = cont;            // Insert text
+document.getElementById("demo3__before").appendChild(ele); 
+ele = document.createElement("P");         // Create a <p> element
+cont = lear_tears.join('****');
+ele.innerHTML = cont;           // Insert text
+document.getElementById("demo3__after").appendChild(ele); 
+  //------------------------------------------------
 
 
-
-console.log(`"Top" is loaded.`);
-console.log(`"Bottom" says: ${funcs[0]}`);
+console.log(`Hit last line of "top.js".`);
 
 // Add suggested array  data1, data2
 // Wire the data into the frontend.
